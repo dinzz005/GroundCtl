d2315a3a81d23aa628ae27fc8e49d6f9993b2705
dinzz005 <dinzzdluffy005@gmail.com>
2026-02-21 13:23:19 +0530

    feat: finding manifest file in the current directory.

---
+++ cmd/groundctl/internals/config/finder.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+var filenames = []string{
+	"groundctl.yaml",
+	"groundctl.yml",
+	".groundctl.yaml",
+	".groundctl.yml",
+}
+
+func FindManifest() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	for _, name := range filenames {
+		path := filepath.Join(dir, name)
+		if stat, err := os.Stat(path); err == nil && !stat.IsDir() {
+			return path, nil
+		}
+	}
+	return "", errors.New("config file not found.")
+
+}
---
+++ cmd/up.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/dinzz005/GroundCtl/cmd/groundctl/internals/config"
+	"github.com/spf13/cobra"
+)
+
+// upCmd represents the up command
+var upCmd = &cobra.Command{
+	Use:   "up",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// calling the config finder to find the manifest file
+		filePath, err := config.FindManifest()
+		if err != nil {
+			cmd.PrintErrf("Error finding manifest: %v\n", err)
+			return
+		}
+		cmd.Println("found manifest file at:", filePath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(upCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// upCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// upCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
